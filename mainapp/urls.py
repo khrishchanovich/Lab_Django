abfad17632c291a41cede9caebc9532f65454926
khrishchanovich <orannon@gmail.com>
2023-05-28 23:46:37 +0300

    Modified urls.py and views.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('contract/', contract, name='contract'),
     path('pay/', pay, name='pay'),
     path('login/', login, name='login'),
-    path('post/<int:post_id>/', show_info, name='post')
+    path('post/<int:post_id>/', show_info, name='post'),
+    path('category/<int:cat_id>/', show_cat, name='category')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 
 from .models import *
 
-menu = [{'title': "Адреса комании", 'url_name': 'address'},
+menu = [{'title': "Адреса компании", 'url_name': 'address'},
         {'title': "Заключить договор", 'url_name': 'contract'},
         {'title': "Оплата", 'url_name': 'pay'},
         {'title': "Личный кабинет", 'url_name': 'login'}
@@ -12,10 +12,14 @@ menu = [{'title': "Адреса комании", 'url_name': 'address'},
 
 def index(request):
     posts = InsuranceType.objects.all()
+    cats = Category.objects.all()
+
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu,
-        'title': 'Виды страхования'
+        'title': 'Виды страхования',
+        'cat_selected': 0
     }
 
     return render(request, 'mainapp/index.html', context=context)
@@ -43,4 +47,22 @@ def login(request):
 
 def show_info(request, post_id):
     return HttpResponse(f'Отображение информации о виде страхования с id = {post_id}')
+
+
+def show_cat(request, cat_id):
+    posts = InsuranceType.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id
+    }
+
+    return render(request, 'mainapp/index.html', context=context)
 
