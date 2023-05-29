70a8cd11f55a95dc270cd5ff6e1ce297b2a8bc2d
khrishchanovich <orannon@gmail.com>
2023-05-29 22:12:06 +0300

    Update db

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,27 +3,39 @@ from django.urls import reverse
 
 
 class InsuranceType(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    content = models.TextField(blank=True, null=True)
-    rate = models.DecimalField(max_digits=10, decimal_places=2)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    description = models.TextField(blank=True, verbose_name='Краткое описание')
+    content = models.TextField(blank=True, null=True, verbose_name='Описание вида страхования')
+    rate = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Тариф')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    is_published = models.BooleanField(default=True, verbose_name='Публикация')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post', kwargs={'post_id': self.pk})
+        return reverse('post', kwargs={'post_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Вид страхования'
+        verbose_name_plural = 'Виды страхования'
+        ordering = ['cat_id', 'title']
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('category', kwargs={'cat_id': self.pk})
+        return reverse('category', kwargs={'cat_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
